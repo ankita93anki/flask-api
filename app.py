0ad4dfbd1c1abb7b36dd5dbd1dfc4fdebec6ccdf
Ankita Singh <ankitasingh7639369@gmail.com>
2024-10-10 21:47:34 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Hello  World"
+
+@app.route("/home")
+def home():
+    return "Home  World"
+
+# import controller.user_controller as user_controller
+# import controller.product_controller as product_controller
+
+from controller import product_controller, user_controller
