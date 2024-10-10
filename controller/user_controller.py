aac596c47b7cd09aa6ba0f1773ce1c66952898ae
Ankita Singh <ankitasingh7639369@gmail.com>
2024-10-10 22:20:05 +0530

    Models

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,4 +1,6 @@
 from app import app
+from model.user_model import user_model
+obj = user_model()
 @app.route("/user/signup")
-def signup():
-    return "This is signup operation"
+def user_signup_controller():
+    return obj.user_signup_model()
---
+++ model/user_model.py
@@ -0,0 +1,3 @@
+class user_model():
+    def user_signup_model(Self):
+        return "This is user_signup_model"
