b378f4b4e04d9ca944255f6f4098186e25431f86
Ankita Singh <ankitasingh7639369@gmail.com>
2024-10-14 21:42:34 +0530

    get api

--- model/user_model.py
+++ model/user_model.py
@@ -1,3 +1,20 @@
+import mysql.connector
+import json
 class user_model():
-    def user_signup_model(Self):
-        return "This is user_signup_model"
+    def __init__(self):
+        #connection establishment code
+        try:
+            self.con = mysql.connector.connect(host="localhost",username="root", password="" , database="flask_tutorial")
+            self.cur=self.con.cursor(dictionary=True)
+            print("connection successful")
+        except:
+            print("some error")
+
+    def user_getall_model(self):
+        #query execution code
+        self.cur.execute("SELECT *FROM users")
+        result = self.cur.fetchall()
+        if len(result)>0:
+            return json.dumps(result)
+        else:
+            return "No Data Found"
